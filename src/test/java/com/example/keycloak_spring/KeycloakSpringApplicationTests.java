df006e09804ba1ed85f884b54235a76f5d40346f
farzadafi <farzadafi40@gmail.com>
2022-09-18 21:36:16 +0430

    In the name of God :)

---
+++ src/test/java/com/example/keycloak_spring/KeycloakSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
