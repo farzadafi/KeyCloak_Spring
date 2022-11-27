0087253e69a2670e72cec7fede28034f584a1692
farzadafi <farzadafi40@gmail.com>
2022-11-27 16:14:17 +0330

    add some field the like Role Object in keycloak

--- src/main/java/com/example/keycloak_spring/model/Role.java
+++ src/main/java/com/example/keycloak_spring/model/Role.java
@@ -1,4 +1,27 @@
 package com.example.keycloak_spring.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+import java.util.UUID;
+
 public class Role {
+    @Id
+    @GeneratedValue(generator = "hibernate_uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(unique = true,nullable = false, columnDefinition = "VARCHAR(36)")
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+    private String name;
+
+    private String realm_id;
+
+    @ManyToMany
+    private Set<User> users;
 }
