09786791ce4d7a5c0e66a5f1559aedae8773c757
farzadafi <farzadafi40@gmail.com>
2022-11-27 16:28:03 +0330

    add needed field for connect to keycloak

--- src/main/java/com/example/keycloak_spring/config/KeycloakConfig.java
+++ src/main/java/com/example/keycloak_spring/config/KeycloakConfig.java
@@ -1,4 +1,13 @@
 package com.example.keycloak_spring.config;
 
+import org.keycloak.admin.client.Keycloak;
+
 public class KeycloakConfig {
+
+    static Keycloak keycloak = null;
+    final static String serverUrl = "http://0.0.0.0:8080/"; //keycloak domain and port
+    public final static String realm = "realm";
+    final static String clientId = "clientId";
+    final static String userName = "admin";
+    final static String password = "admin";
 }
