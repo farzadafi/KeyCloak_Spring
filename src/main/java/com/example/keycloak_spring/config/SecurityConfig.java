f738d632abac3cdae504f2d4a6ab6737b4969685
farzadafi <farzadafi40@gmail.com>
2022-11-27 16:35:50 +0330

    Override configure method and implement body

--- src/main/java/com/example/keycloak_spring/config/SecurityConfig.java
+++ src/main/java/com/example/keycloak_spring/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.keycloak_spring.config;
 
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
@@ -8,5 +9,14 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return null;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.cors().and().csrf().disable().authorizeRequests()
+                //.anyRequest().authenticated().and().formLogin(); //for form login
+                .anyRequest().permitAll();
     }
 }
