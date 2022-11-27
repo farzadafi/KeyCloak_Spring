4646b0a75c086c69f6fa6a230ec78923fa5b2f93
farzadafi <farzadafi40@gmail.com>
2022-11-27 16:16:38 +0330

    add some field like UserAttribute Object in keycloak

--- src/main/java/com/example/keycloak_spring/model/UserAttribute.java
+++ src/main/java/com/example/keycloak_spring/model/UserAttribute.java
@@ -1,4 +1,23 @@
 package com.example.keycloak_spring.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
 public class UserAttribute {
+    @Id
+    @GeneratedValue(generator = "hibernate_uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(unique = true,nullable = false, columnDefinition = "VARCHAR(36)")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String value;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
