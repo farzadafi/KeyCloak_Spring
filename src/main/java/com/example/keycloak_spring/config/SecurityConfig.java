842d37d08bec490e102fadc7cdc7b0792ea07427
farzadafi <farzadafi40@gmail.com>
2022-11-27 16:37:16 +0330

    add buildSessionRegistry method for Bean

--- src/main/java/com/example/keycloak_spring/config/SecurityConfig.java
+++ src/main/java/com/example/keycloak_spring/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.keycloak_spring.config;
 
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
+    @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return null;
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
+
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
     }
 
     @Override
