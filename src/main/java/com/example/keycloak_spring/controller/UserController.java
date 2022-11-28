7c4649101db0593d2343dece20a12980777f1d44
farzadafi <farzadafi40@gmail.com>
2022-11-28 18:13:50 +0330

    added registerUser method

--- src/main/java/com/example/keycloak_spring/controller/UserController.java
+++ src/main/java/com/example/keycloak_spring/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.example.keycloak_spring.controller;
 
+import com.example.keycloak_spring.dto.UserDto;
+import com.example.keycloak_spring.service.user.UserServiceImpel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserServiceImpel userService;
+
+    public UserController(UserServiceImpel userServiceImpel) {
+        this.userService = userServiceImpel;
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(@RequestBody UserDto userDto) {
+        userService.registerUser(userDto);
+        return "OK";
+    }
 }
