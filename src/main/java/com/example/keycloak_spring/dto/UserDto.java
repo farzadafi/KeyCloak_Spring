77003af8fbf564bb25da58793b278ee2b310ccfe
farzadafi <farzadafi40@gmail.com>
2022-11-28 18:00:24 +0330

    added validation for each field

--- src/main/java/com/example/keycloak_spring/dto/UserDto.java
+++ src/main/java/com/example/keycloak_spring/dto/UserDto.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -11,13 +15,46 @@ public class UserDto {
 
     private String id;
 
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{3,20}$",
+            message = "please enter a valid first name(just alphabetic)")
     private String firstname;
 
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{3,20}$",
+            message = "please enter a valid first name(just alphabetic)")
     private String lastname;
 
+    @NotNull
+    @Pattern(regexp = "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$",
+            message = "please enter a valid user name(just alphabetic or numeric char)")
     private String username;
 
+    @NotNull
+    @Email
     private String email;
 
+    @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&#^])[A-Za-z0-9@$!%*?&]{8,10}$",
+            message = "password must :Minimum 8, maximum 10 char, at least one uppercase, one lowercase, one number and one (!@#$%^&*) ")
+    private String password;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[0-9])[0-9]{10}",
+            message = "please enter just 10 number")
     private String meliCode;
+
+    /*
+    regex pattern for user name
+^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$
+ └─────┬────┘└───┬──┘└─────┬─────┘└─────┬─────┘ └───┬───┘
+       │         │         │            │           no _ or . at the end
+       │         │         │            │
+       │         │         │            allowed characters
+       │         │         │
+       │         │         no __ or _. or ._ or .. inside
+       │         │
+       │         no _ or . at the beginning
+       │
+       username is 8-20 characters long*/
 }
