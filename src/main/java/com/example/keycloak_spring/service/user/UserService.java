db2e32e86143b23f638dccf43eba8b783a357fc3
farzadafi <farzadafi40@gmail.com>
2022-11-28 18:05:11 +0330

    added getInstance method

--- src/main/java/com/example/keycloak_spring/service/user/UserService.java
+++ src/main/java/com/example/keycloak_spring/service/user/UserService.java
@@ -1,4 +1,7 @@
 package com.example.keycloak_spring.service.user;
 
+import org.keycloak.admin.client.resource.UsersResource;
+
 public interface UserService {
+    UsersResource getInstance();
 }
--- src/main/java/com/example/keycloak_spring/service/user/UserServiceImpel.java
+++ src/main/java/com/example/keycloak_spring/service/user/UserServiceImpel.java
@@ -1,4 +1,12 @@
 package com.example.keycloak_spring.service.user;
 
+import com.example.keycloak_spring.config.KeycloakConfig;
+import org.keycloak.admin.client.resource.UsersResource;
+
 public class UserServiceImpel implements UserService{
+
+    @Override
+    public UsersResource getInstance() {
+        return KeycloakConfig.getInstance().realm(KeycloakConfig.realm).users();
+    }
 }
