1b58b63f327aa820701ac1a7d702ac7255c2cbb0
farzadafi <farzadafi40@gmail.com>
2022-11-28 17:50:29 +0330

    add needed field and lombok annotation

--- src/main/java/com/example/keycloak_spring/dto/UserDto.java
+++ src/main/java/com/example/keycloak_spring/dto/UserDto.java
@@ -1,4 +1,23 @@
 package com.example.keycloak_spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserDto {
+
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String username;
+
+    private String email;
+
+    private String meliCode;
 }
