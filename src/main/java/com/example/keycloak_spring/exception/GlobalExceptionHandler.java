5729587069c79abed3f16553e3d9a354f887185a
farzadafi <farzadafi40@gmail.com>
2022-11-28 18:44:14 +0330

    added failSaveExceptionHandler method

--- src/main/java/com/example/keycloak_spring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/keycloak_spring/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.example.keycloak_spring.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FailSaveException.class)
+    public ResponseEntity<String> failSaveExceptionHandler(FailSaveException u) {
+        log.error(u.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(u.getMessage());
+    }
 }
