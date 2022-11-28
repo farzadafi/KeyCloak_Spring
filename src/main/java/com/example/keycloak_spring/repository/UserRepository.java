6db1da5d6421843ebe1fe871f675f81c2f8c9f0a
farzadafi <farzadafi40@gmail.com>
2022-11-28 18:47:44 +0330

    extends from JpaRepository interface

--- src/main/java/com/example/keycloak_spring/repository/UserRepository.java
+++ src/main/java/com/example/keycloak_spring/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.keycloak_spring.repository;
 
-public interface UserRepository {
+import com.example.keycloak_spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
