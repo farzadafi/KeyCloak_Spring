9b31a4d2251ff951b40d0f4788d5f641d9c6bc1b
farzadafi <farzadafi40@gmail.com>
2022-11-29 16:04:47 +0330

    added findUserByUsername method

--- src/main/java/com/example/keycloak_spring/repository/UserRepository.java
+++ src/main/java/com/example/keycloak_spring/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.keycloak_spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findUserByUsername(String username);
 }
