f1bf31a6a12db91e4430c9f4148acb7f261bdb31
farzadafi <farzadafi40@gmail.com>
2022-11-30 13:43:16 +0330

    add addRole method

--- src/main/java/com/example/keycloak_spring/controller/RoleController.java
+++ src/main/java/com/example/keycloak_spring/controller/RoleController.java
@@ -1,4 +1,25 @@
 package com.example.keycloak_spring.controller;
 
+import com.example.keycloak_spring.dto.RoleDto;
+import com.example.keycloak_spring.service.role.RoleServiceImpel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+
+    private final RoleServiceImpel roleService;
+
+    public RoleController(RoleServiceImpel roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/addRole")
+    public String addRole(@RequestBody RoleDto roleDto) {
+        roleService.addRole(roleDto);
+        return "OK";
+    }
 }
