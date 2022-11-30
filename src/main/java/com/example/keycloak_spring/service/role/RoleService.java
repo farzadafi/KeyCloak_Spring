3e5478186b03bd6d3b53727502c84e6e3ef20d36
farzadafi <farzadafi40@gmail.com>
2022-11-30 13:31:26 +0330

    added getInstance method

--- src/main/java/com/example/keycloak_spring/service/role/RoleService.java
+++ src/main/java/com/example/keycloak_spring/service/role/RoleService.java
@@ -1,4 +1,7 @@
 package com.example.keycloak_spring.service.role;
 
+import org.keycloak.admin.client.resource.RolesResource;
+
 public interface RoleService {
+    RolesResource getInstance();
 }
--- src/main/java/com/example/keycloak_spring/service/role/RoleServiceImpel.java
+++ src/main/java/com/example/keycloak_spring/service/role/RoleServiceImpel.java
@@ -1,4 +1,12 @@
 package com.example.keycloak_spring.service.role;
 
+import com.example.keycloak_spring.config.KeycloakConfig;
+import org.keycloak.admin.client.resource.RolesResource;
+
 public class RoleServiceImpel implements RoleService{
+
+    @Override
+    public RolesResource getInstance() {
+        return KeycloakConfig.getInstance().realm(KeycloakConfig.REALM).roles();
+    }
 }
