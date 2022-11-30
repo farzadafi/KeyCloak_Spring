726e37cdb76e2aa2faef045b36cb2eb64d055192
farzadafi <farzadafi40@gmail.com>
2022-11-30 13:33:23 +0330

    add some fields according to Role class

--- src/main/java/com/example/keycloak_spring/dto/RoleDto.java
+++ src/main/java/com/example/keycloak_spring/dto/RoleDto.java
@@ -1,4 +1,15 @@
 package com.example.keycloak_spring.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class RoleDto {
+
+    private String name;
+    private String description;
 }
