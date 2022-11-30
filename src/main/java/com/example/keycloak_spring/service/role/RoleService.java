8d929e09ca7375e02d123a50f3990938046dd172
farzadafi <farzadafi40@gmail.com>
2022-11-30 13:34:46 +0330

    add addRole method

--- src/main/java/com/example/keycloak_spring/service/role/RoleService.java
+++ src/main/java/com/example/keycloak_spring/service/role/RoleService.java
@@ -1,7 +1,10 @@
 package com.example.keycloak_spring.service.role;
 
+import com.example.keycloak_spring.dto.RoleDto;
 import org.keycloak.admin.client.resource.RolesResource;
 
 public interface RoleService {
     RolesResource getInstance();
+
+    void addRole(RoleDto roleDto);
 }
--- src/main/java/com/example/keycloak_spring/service/role/RoleServiceImpel.java
+++ src/main/java/com/example/keycloak_spring/service/role/RoleServiceImpel.java
@@ -1,12 +1,24 @@
 package com.example.keycloak_spring.service.role;
 
 import com.example.keycloak_spring.config.KeycloakConfig;
+import com.example.keycloak_spring.dto.RoleDto;
 import org.keycloak.admin.client.resource.RolesResource;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 public class RoleServiceImpel implements RoleService{
 
     @Override
     public RolesResource getInstance() {
         return KeycloakConfig.getInstance().realm(KeycloakConfig.REALM).roles();
+    }
+
+    @Override
+    public void addRole(RoleDto roleDto) {
+        RolesResource rolesResource = getInstance();
+
+        RoleRepresentation roleRepresentation = new RoleRepresentation(roleDto.getName(),
+                roleDto.getDescription(),
+                true);
+        rolesResource.create(roleRepresentation);
     }
 }
