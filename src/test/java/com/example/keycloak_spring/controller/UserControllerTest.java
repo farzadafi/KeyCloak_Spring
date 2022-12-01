5d664374abdd63ff56db8841ff1e9f6d21cf6569
farzadafi <farzadafi40@gmail.com>
2022-12-01 08:45:29 +0330

    create UserControllerTest class

---
+++ src/test/java/com/example/keycloak_spring/controller/UserControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.keycloak_spring.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+}
