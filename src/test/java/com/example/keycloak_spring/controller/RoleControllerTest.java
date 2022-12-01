91fc236d6cbe78d082df7a0453baa4fed665bc82
farzadafi <farzadafi40@gmail.com>
2022-12-01 08:55:23 +0330

    create RoleControllerTest class

---
+++ src/test/java/com/example/keycloak_spring/controller/RoleControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.keycloak_spring.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleControllerTest {
+
+    @Test
+    void addRole() {
+    }
+
+    @Test
+    void getAllRole() {
+    }
+
+    @Test
+    void removeRole() {
+    }
+}
